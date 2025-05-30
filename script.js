75d826a4ed1227109e9351e28bbd768d62511987
Dhanush Kunchakuri <132380514+dhanushkunchakuri@users.noreply.github.com>
2025-05-30 17:50:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function highlight() {
-    //Write your code here
-
+    //Write your code her
+		document.querySelectorAll("strong").forEach((item) =>{
+			item.style.color="rgb(0, 128, 0)";
+		});
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+	document.querySelectorAll("strong").forEach((item)=>{
+		item.style.color="rgb(0,0,0)";
+	})
     
 }
